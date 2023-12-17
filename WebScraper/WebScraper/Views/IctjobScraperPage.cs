97eb38539750fbbb9eaba31ce763d4a9f92f5a4c
Guy De Roover <57800899+GuyDeRoover@users.noreply.github.com>
2023-12-17 16:05:26 +0100

    Upload files

---
+++ WebScraper/WebScraper/Views/IctjobScraperPage.cs
@@ -0,0 +1,16 @@
+﻿
+namespace WebScraper.Views
+{
+    static class IctjobScraperPage
+    {
+        public static void Print()
+        {
+            Console.Clear();
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine("Ictjob Web Scraper");
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.Write("Enter the keyword(s) for your scrape: ");
+        }
+    }
+}
